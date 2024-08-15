cd90af637d18953ace30a0fc32729631a78a4ca3
Lindsay Terry <lindsaytee66@gmail.com>
2024-08-15 17:07:51 -0600

    Adds thought and reaction schemas

---
+++ models/Reaction.js
@@ -0,0 +1,16 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: { type: Schema.Types.ObjectId, default: new ObjectId },
+        reactionBody: { type: String, required: true, maxLength: [280] },
+        username: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+    }
+);
+
+reactionSchema
+    .virtual('timestampFormat')
+    .get(function () {
+        return this.createdAt.toLocaleDateString('MM/DD/YYYY');
+    });
