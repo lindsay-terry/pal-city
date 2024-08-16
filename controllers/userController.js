cdc0f42109b920a76dbbadfa194474863bbf4884
Lindsay Terry <lindsaytee66@gmail.com>
2024-08-15 23:09:45 -0600

    Adds routes to server.js and fixes typo in getSingleUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,7 +19,7 @@ module.exports = {
             .select('-__v');
 
             if (!user) {
-                return res.status(404).json({ message: 'NO user found with that ID.' });
+                return res.status(404).json({ message: 'No user found with that ID.' });
             }
             res.status(200).json(user);
         } catch (error) {
